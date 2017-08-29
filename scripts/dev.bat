f1b5143aea7f7087ad17e1bf5d54ea33a9830307
Dan <danielmillst@gmail.com>
2017-08-29 09:12:01 -0400

    project

---
+++ scripts/dev.bat
@@ -0,0 +1,3 @@
+@Echo off
+echo Copying Vitality to development server
+xcopy /y C:\Users\cyberpwn\Documents\development\workspace\Vitality\target\Vitality.jar C:\Users\cyberpwn\Documents\development\servers\dynamic\plugins
