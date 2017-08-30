db4dceb8e71cde79caa3701e2b778cde73d3f397
Dan <danielmillst@gmail.com>
2017-08-30 14:35:54 -0400

    Vitality feature

---
+++ src/main/java/org/cyberpwn/vitality/feature/FeatureVitality.java
@@ -0,0 +1,23 @@
+package org.cyberpwn.vitality.feature;
+
+import org.bukkit.command.CommandSender;
+import org.cyberpwn.vitality.command.Command;
+import org.cyberpwn.vitality.util.ProtocolRange;
+
+public class FeatureVitality extends VitalFeature
+{
+	public FeatureVitality(ProtocolRange range)
+	{
+		super(range);
+	}
+	
+	public void showHelp(CommandSender sender)
+	{
+		
+	}
+	
+	public void showHelp(CommandSender sender, Command command)
+	{
+		
+	}
+}
